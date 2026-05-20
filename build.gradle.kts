214ab02584a51fda574b9263a92d91426102bed3
brahmanbob <brahmanbob@gmail.com>
2026-05-20 16:03:32 +1000

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file
+plugins {
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
